641bd2f3f833ab96c5d7248e6a2c2d14f13b497f
niing <t.nuttawipha@gmail.com>
2025-11-28 23:25:37 +0700

    feat: adding movie list

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import "./App.css";
+import movies from "./data/movies";
 
 function App() {
   return (
     <div className="App">
       <section className="movie-list-section">
+        <h1> Movie List Section </h1>
         {/* Render Movie Lists Here */}
+          {movies.map((item,index)=>{
+            return  <div className="movieBox" key={index}> 
+                        <img src={item.image} alt="Movie Poster" />
+                      <div className="detailBox">
+                        <p> Title: {item.title} </p>
+                        <p> Year: {item.year} </p>
+                        <p> Runtime: {item.runtime} </p>
+                        <p> Genres:
+                          {item.genres.map((genre)=>(
+                              <span key={genre} className="genreStyle"> {genre} </span>
+                          ))} 
+                        </p>
+                        <p> IMDB Rating: {item.imdbRating} </p>
+                        <p> IMDB Vote: {item.imdbVotes} </p>
+                      </div>
+                    </div>  
+          })}
       </section>
     </div>
   );
